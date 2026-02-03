11e6bdd649d5692d4c266c47e425f9fba287f8ac
Doniyorbek <142497625+DevteachCoder@users.noreply.github.com>
2026-02-03 15:16:59 +0500

    Add files via upload

---
+++ Objects properties+methods/js/script.js
@@ -0,0 +1,130 @@
+// const name = "Doniyor"
+
+// let user = new Object();
+// console.log(typeof user);
+
+// let user = {
+//     name:"Doniyor",
+//     age: 24,
+// };
+// user.email = "doni@gmail.com";
+// console.log(user);
+
+// alert(user.age);
+// console.log(user.name);
+
+// const myString = "Hello World";
+//  let a = myString.length;
+//  console.log(a);
+ 
+
+// let user = {
+//     name:"Doniyor",
+//     age: 24,
+//     location:"Karshi"
+// };
+// delete user.location;
+// console.log(user);
+
+// let anObject = {left:2,right:2}
+// console.log(anObject.left);
+// delete anObject.left;
+
+// console.log("left" in anObject);
+
+
+// let user = {
+//     name:"Doni",
+//     age:24,
+//     email:"doni@gmail.com",
+//     location:"Karshi",
+//     hobby:["read book","play game","foodball"],
+//     socialMedia:{
+//         instagram:"DonikCoding",
+//         faacebook:"doni.dev",
+//         telegram: "doniyorkuziev"
+//     }
+// }
+// let a = user.hobby[0];
+// console.log(a);
+
+// let a = "Hello";
+// console.log(a.toUpperCase());
+
+// let s = [1,2,3];
+// // s.push(4);
+// // s.pop(2)
+// console.log(s);
+
+// user.login();
+// console.log(Object.keys(user));
+
+// let ObjA = {
+//     a:1,
+//     b:2
+// }
+// let ObjB = {
+//     c:3,
+//     d:4
+// }
+// Object.assign(ObjA,ObjB);  // assign ==== 2 ta objectlarni qo'shib beradi!!
+// console.log(ObjA);
+
+// let obj1 = {message: 10}
+// let obj2 = {message: 10}
+// console.log(obj2 == obj1);
+
+// console.log(this.obj1);
+
+
+// let user = {
+//     name:"Doni",
+//     age:24,
+//     email:"doni@gmail.com",
+//     location:"Karshi",
+//     hobby:["read book","play game","foodball"],
+//       socialMedia:{
+//          instagram:"DonikCoding",
+//          faacebook:"doni.dev",
+//          telegram: "doniyorkuziev"
+//     }
+//  }
+
+//  console.log(this.hobby.forEach(item.index)){}
+
+
+// LOOP
+
+// const foodArray = [
+//     {name:"Pizza"},
+//     {name:"Burger"},
+//     {name:"Plov"},
+// ]
+// for(let food of foodArray) {
+//     console.log(`food name : ${food.name}`);
+    
+// }
+
+// let oneArray = ["oq","kuk","sariq","qora"]
+// let a = 10
+
+// for(let one of oneArray) {       //of
+//     console.log(one +" "+ a++);
+// }
+
+// for(let two in oneArray) {       //in
+//     console.log(two);
+// }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
